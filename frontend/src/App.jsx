b7b29f9a93bbb609b444d73fa2b28037847f36ce
ClaireBayi <ngueuleuachiclaire@gmail.com>
2026-05-12 02:36:17 +0100

    Fix route LandingPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
-import LandingPage from './pages/LandingPage';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import DashboardClient from './pages/DashboardClient';
@@ -7,11 +7,9 @@ import DashboardLivreur from './pages/DashboardLivreur';
 import DashboardAdmin from './pages/DashboardAdmin';
 import NouvelleCommande from './pages/NouvelleCommande';
 
-
 const PrivateRoute = ({ children, allowedRoles }) => {
   const token = localStorage.getItem('token');
   const user = JSON.parse(localStorage.getItem('user'));
-
   if (!token) return <Navigate to="/login" />;
   if (allowedRoles && !allowedRoles.includes(user?.role)) {
     return <Navigate to="/login" />;
@@ -22,50 +20,14 @@ const PrivateRoute = ({ children, allowedRoles }) => {
 export default function App() {
   return (
     <BrowserRouter>
-    <Route path="/" element={<LandingPage />} />
       <Routes>
-
-        {/* Publiques */}
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-
-        {/* Client */}
-        <Route
-          path="/dashboard/client"
-          element={
-            <PrivateRoute allowedRoles={['CLIENT']}>
-              <DashboardClient />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="/commandes/new"
-          element={
-            <PrivateRoute allowedRoles={['CLIENT']}>
-              <NouvelleCommande />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Livreur */}
-        <Route
-          path="/dashboard/livreur"
-          element={
-            <PrivateRoute allowedRoles={['LIVREUR']}>
-              <DashboardLivreur />
-            </PrivateRoute>
-          }
-        />
-
-        {/* Admin */}
-        <Route
-          path="/dashboard/admin"
-          element={
-            <PrivateRoute allowedRoles={['ADMIN']}>
-              <DashboardAdmin />
-            </PrivateRoute>
-          }
-        />
+        <Route path="/dashboard/client" element={<PrivateRoute allowedRoles={['CLIENT']}><DashboardClient /></PrivateRoute>} />
+        <Route path="/commandes/new" element={<PrivateRoute allowedRoles={['CLIENT']}><NouvelleCommande /></PrivateRoute>} />
+        <Route path="/dashboard/livreur" element={<PrivateRoute allowedRoles={['LIVREUR']}><DashboardLivreur /></PrivateRoute>} />
+        <Route path="/dashboard/admin" element={<PrivateRoute allowedRoles={['ADMIN']}><DashboardAdmin /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   );
